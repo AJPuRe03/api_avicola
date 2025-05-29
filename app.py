91e9befb552912d74b29bfbf42cdce0b20187dd8
Angel <127281111+AJPuRe03@users.noreply.github.com>
2025-05-29 12:34:19 -0600

    Agregando CORS

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from dotenv import load_dotenv
 
@@ -8,6 +9,7 @@ load_dotenv()
 
 #crear instancia
 app = Flask(__name__)
+CORS(app)
 
 #Configuración de la base de datos PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
